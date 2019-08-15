599148958ed7980ed23346a1fba48e843b50bb2d
Zhi Tian <tianzhi0549@163.com>
2019-08-15 12:26:01 +0800

    add giou

--- fcos_core/layers/iou_loss.py
+++ fcos_core/layers/iou_loss.py
@@ -3,6 +3,10 @@ from torch import nn
 
 
 class IOULoss(nn.Module):
+    def __init__(self):
+        super(IOULoss, self).__init__()
+        self.loc_loss_type = "giou"
+
     def forward(self, pred, target, weight=None):
         pred_left = pred[:, 0]
         pred_top = pred[:, 1]
@@ -19,18 +23,28 @@ class IOULoss(nn.Module):
         pred_area = (pred_left + pred_right) * \
                     (pred_top + pred_bottom)
 
-        w_intersect = torch.min(pred_left, target_left) + \
-                      torch.min(pred_right, target_right)
-        h_intersect = torch.min(pred_bottom, target_bottom) + \
-                      torch.min(pred_top, target_top)
-
+        w_intersect = torch.min(pred_left, target_left) + torch.min(pred_right, target_right)
+        g_w_intersect = torch.max(pred_left, target_left) + torch.max(
+            pred_right, target_right)
+        h_intersect = torch.min(pred_bottom, target_bottom) + torch.min(pred_top, target_top)
+        g_h_intersect = torch.max(pred_bottom, target_bottom) + torch.max(pred_top, target_top)
+        ac_uion = g_w_intersect * g_h_intersect + 1e-7
         area_intersect = w_intersect * h_intersect
         area_union = target_area + pred_area - area_intersect
-
-        losses = -torch.log((area_intersect + 1.0) / (area_union + 1.0))
+        ious = (area_intersect + 1.0) / (area_union + 1.0)
+        gious = ious - (ac_uion - area_union) / ac_uion
+        if self.loc_loss_type == 'iou':
+            losses = -torch.log(ious)
+        elif self.loc_loss_type == 'linear_iou':
+            losses = 1 - ious
+        elif self.loc_loss_type == 'giou':
+            losses = 1 - gious
+        else:
+            raise NotImplementedError
 
         if weight is not None and weight.sum() > 0:
             return (losses * weight).sum() / weight.sum()
         else:
             assert losses.numel() != 0
             return losses.mean()
+
