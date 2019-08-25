0e6e0004bd9b09095c8746f9c3d638794c8b5d51
Zhi Tian <tianzhi0549@163.com>
2019-08-25 16:25:24 +0930

    maskrcnn_benchmark -> fcos_core

--- fcos_core/layers/dcn/deform_pool_func.py
+++ fcos_core/layers/dcn/deform_pool_func.py
@@ -2,7 +2,7 @@ import torch
 from torch.autograd import Function
 from torch.autograd.function import once_differentiable
 
-from maskrcnn_benchmark import _C
+from fcos_core import _C
 
 
 class DeformRoIPoolingFunction(Function):
