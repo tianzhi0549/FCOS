980660cf86aa8feef6e6bd38d6e5d8c8942eddc1
rram12 <ramzi.raddaoui@esprit.tn>
2022-07-05 16:53:28 +0200

    fix import issues

--- fcos_core/csrc/cpu/ROIAlign_cpu.cpp
+++ fcos_core/csrc/cpu/ROIAlign_cpu.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#include "cpu/vision.h"
+#include "vision.h"
 
 // implementation taken from Caffe2
 template <typename T>
--- fcos_core/utils/model_zoo.py
+++ fcos_core/utils/model_zoo.py
@@ -3,11 +3,11 @@ import os
 import sys
 
 try:
-    from torch.utils.model_zoo import _download_url_to_file
+    from torch.utils.model_zoo import download_url_to_file
     from torch.utils.model_zoo import urlparse
     from torch.utils.model_zoo import HASH_REGEX
 except:
-    from torch.hub import _download_url_to_file
+    from torch.hub import download_url_to_file
     from torch.hub import urlparse
     from torch.hub import HASH_REGEX
 
@@ -59,6 +59,6 @@ def cache_url(url, model_dir=None, progress=True):
             # if the hash_prefix is less than 6 characters
             if len(hash_prefix) < 6:
                 hash_prefix = None
-        _download_url_to_file(url, cached_file, hash_prefix, progress=progress)
+        download_url_to_file(url, cached_file, hash_prefix, progress=progress)
     synchronize()
     return cached_file
